cdbe9627499100c431467db38a0325c87439cc91
Steve@ooyama <steve.nguyen@college-de-france.fr>
2013-04-02 12:39:27 +0200

    emacs initial

---
+++ dot_emacs_auto/insert.hpp
@@ -0,0 +1,22 @@
+//*****************************************************************************
+//
+// File Name	: '&&&'
+// Author	: Steve NGUYEN
+// Contact      : steve.nguyen@college-de-france.fr
+// Created	: DDDD
+// Revised	: 
+// Version	: 
+// Target MCU	: 
+//
+// This code is distributed under the GNU Public License
+//		which can be found at http://www.gnu.org/licenses/gpl.txt
+//
+//
+// Notes:	notes
+//
+//*****************************************************************************
+
+#if !defined(@@@)
+#define @@@
+
+#endif
---
+++ dot_emacs_auto/insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python 
+# -*- coding: utf-8 -*-
+
+########################################################################
+#  File Name	: '@@@'
+#  Author	: Steve NGUYEN
+#  Contact      : steve.nguyen@college-de-france.fr
+#  Created	: DDDD
+#  Revised	: 
+#  Version	: 
+#  Target MCU	: 
+#
+#  This code is distributed under the GNU Public License
+# 		which can be found at http://www.gnu.org/licenses/gpl.txt
+#
+#
+#  Notes:	notes
+########################################################################
