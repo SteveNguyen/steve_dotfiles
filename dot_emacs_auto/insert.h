68722fefe116de0b8033fc678b5b97c4206aa119
Steve Nguyen <steve.nguyen.000@gmail.com>
2014-08-28 14:19:13 +0200

    markers

--- dot_emacs_auto/insert.h
+++ dot_emacs_auto/insert.h
@@ -2,11 +2,11 @@
 //
 // File Name	: '&&&'
 // Author	: Steve NGUYEN
-// Contact      : steve.nguyen@college-de-france.fr
+// Contact      : steve.nguyen@inria.fr
 // Created	: DDDD
-// Revised	: 
-// Version	: 
-// Target MCU	: 
+// Revised	:
+// Version	:
+// Target MCU	:
 //
 // This code is distributed under the GNU Public License
 //		which can be found at http://www.gnu.org/licenses/gpl.txt
--- dot_emacs_auto/insert.py
+++ dot_emacs_auto/insert.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 ########################################################################
 #  File Name	: '@@@'
 #  Author	: Steve NGUYEN
-#  Contact      : steve.nguyen@college-de-france.fr
+#  Contact      : steve.nguyen@inria.fr
 #  Created	: DDDD
-#  Revised	: 
-#  Version	: 
-#  Target MCU	: 
+#  Revised	:
+#  Version	:
+#  Target MCU	:
 #
 #  This code is distributed under the GNU Public License
 # 		which can be found at http://www.gnu.org/licenses/gpl.txt
